6faba473e0f26fb10113fde663c78ca19dcc39bf
FloKnapp <office@florianknapp.de>
2025-05-02 23:24:36 +0200

    Fixed namespace typo

--- bootstrap.php
+++ bootstrap.php
@@ -2,9 +2,9 @@
 
 require_once __DIR__ . '/vendor/autoload.php';
 
-use PHPico\Form\Events\CsrfListener;
-use PHPico\Form\Support\Guard;
-use function PHPico\app;
+use NixPHP\Form\Events\CsrfListener;
+use NixPHP\Form\Support\Guard;
+use function NixPHP\app;
 
 app()->container()->set('guard', function () { return new Guard(); });
 app()->container()->get('event')->listen('controller.calling', [CsrfListener::class, 'handle']);

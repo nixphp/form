1c98e2f355d991f49d17bce97ebaeae4b17521e9
FloKnapp <office@florianknapp.de>
2025-11-30 18:16:42 +0100

    Preparations for v0.1.0 - Clear Validator-Registry after every test

--- tests/NixPHPTestCase.php
+++ tests/NixPHPTestCase.php
@@ -4,9 +4,20 @@ declare(strict_types=1);
 
 namespace Tests;
 
+use NixPHP\Form\Core\Validator;
 use PHPUnit\Framework\TestCase;
 
 class NixPHPTestCase extends TestCase
 {
+
+    protected function tearDown(): void
+    {
+        // Reset the static registry after each test
+        $reflection = new \ReflectionClass(Validator::class);
+        $property = $reflection->getProperty('registry');
+        $property->setAccessible(true);
+        $property->setValue(null, []);
+        parent::tearDown();
+    }
 
 }
